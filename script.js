6feab822ca469c95a10645ea3e061cd84158f84b
elzat2013i-commits <elzat2013i@gmail.com>
2025-12-02 10:11:02 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let sutent ={
+    name:'elzat',
+    age:6,
+    class:["1 class"],
+    year:2018,
+    hobby:'играть'
+}
+sutent.age = 13
+sutent.class = "7 class"
+sutent.hobby = 'читать,рисовать,смотреть что-либо'
+sutent.year = 2025
+console.log(sutent)
